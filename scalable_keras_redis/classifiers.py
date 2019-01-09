11e89acbdfa0135ed0b81d2c8e0f9ce7adf22711
rputra <rhpmail@gmail.com>
2019-01-09 16:00:56 +0700

    support loading ensemble model

--- scalable_keras_redis/classifiers.py
+++ scalable_keras_redis/classifiers.py
@@ -8,6 +8,12 @@ from scipy.misc import imresize
 import os
 import path_config
 from keras.optimizers import Adam
+from keras.models import Model
+from keras.layers import Input, Dense, Flatten, Dropout, average
+from sklearn.model_selection import train_test_split
+from keras.applications.inception_v3 import InceptionV3
+from keras.applications.resnet50 import ResNet50
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
 
 IMAGE_WIDTH = 224
 IMAGE_HEIGHT = 224
@@ -55,6 +61,43 @@ class Pred_Model_Base(object):
                      metrics=['accuracy'])
         print('load {0}'.format(model_weight_file))
 
+    def load_ensemble_model(self, model_weight_file):
+        input_shape = [224,224,3]
+
+        inceptionV3_arch = InceptionV3(include_top=False, weights=None, pooling='avg')
+        in_layer = Input(shape=input_shape, name='image_input')
+        x = inceptionV3_arch(in_layer)
+        prediction = Dense(1, activation='sigmoid')(x)
+        inceptionV3 = Model(inputs=in_layer, outputs=prediction)
+
+        resnet50_arch = ResNet50(include_top=False, weights=None, pooling='avg')
+        in_layer = Input(shape=input_shape, name='image_input')
+        x = resnet50_arch(in_layer)
+        prediction = Dense(1, activation='sigmoid')(x)
+        resnet50 = Model(inputs=in_layer, outputs=prediction)
+
+        inceptionResNet_arch = InceptionResNetV2(include_top=False, weights=None, pooling='avg')
+        in_layer = Input(shape=input_shape, name='image_input')
+        x = inceptionResNet_arch(in_layer)
+        prediction = Dense(1, activation='sigmoid')(x)
+        inceptionResNet = Model(inputs=in_layer, outputs=prediction)
+
+        inceptionV3.name = 'InceptionV3'
+        resnet50.name = 'ResNet50'
+        inceptionResNet.name = 'InceptionResNetV2'
+
+        in_layer = Input(shape=input_shape, name='image_input')
+        out_inceptionV3 = inceptionV3(in_layer)
+        out_resnet50 = resnet50(in_layer)
+        out_inceptionResNet = inceptionResNet(in_layer)
+        out_models = [out_inceptionV3, out_resnet50, out_inceptionResNet]
+        prediction = average(out_models)
+        ensemble_model = Model(inputs=in_layer, outputs=prediction, name='ensemble')
+        ensemble_model.summary()
+        ensemble_model.load_weights(model_weight_file)
+        self.model = ensemble_model
+
+
     def _ml_predict(self, image):
         prediction = -1
         with self.graph.as_default():
@@ -97,14 +140,16 @@ class Alcohol_Model(Pred_Model_Base):
     
     def __init__(self):
         Pred_Model_Base.__init__(self)
-        json_file=path_config.alcohol_json_file
+        #json_file=path_config.alcohol_json_file
         model_weight_file = path_config.alcohol_h5_file
-        self.load_model(json_file, model_weight_file)
+        #self.load_model(json_file, model_weight_file)
+        self.load_ensemble_model(model_weight_file)
 
 class Gambling_Model(Pred_Model_Base):
     
     def __init__(self):
         Pred_Model_Base.__init__(self)
-        json_file=path_config.gambling_json_file
+        #json_file=path_config.gambling_json_file
         model_weight_file = path_config.gambling_h5_file
-        self.load_model(json_file, model_weight_file)
+        #self.load_model(json_file, model_weight_file)
+        self.load_ensemble_model(model_weight_file)
