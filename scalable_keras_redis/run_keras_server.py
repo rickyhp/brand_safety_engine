112089ef1b529c2008e3e8c2552d6621c0245aeb
Ubuntu <ubuntu@ip-172-31-35-98.us-west-2.compute.internal>
2019-01-19 10:09:36 +0000

    remove text integration part in backend code, move to js

--- scalable_keras_redis/run_keras_server.py
+++ scalable_keras_redis/run_keras_server.py
@@ -126,15 +126,13 @@ def get_results():
 def get_final_results():
 	data = json.loads(request.data.decode())
 	print("json: "+json.dumps(data))
-	website = data['website']
 	website_folder = None
 	finalResults = {}
 	font_color = settings.Unknown_Color
 	finalResults[settings.Advice] = settings.Unknown_Value
 	finalResults[settings.Font_Color] = font_color
 	finalResults[settings.Probabilities] = settings.default_result()
-	r = requests.get(settings.Text_url + '/alcohol?url=' + website.strip())
-	finalResults[settings.Category_Alcohol] = r.text
+
 	if settings.Website_Folder_Column in data:
 		website_folder = data[settings.Website_Folder_Column]
 	if website_folder is not None:
