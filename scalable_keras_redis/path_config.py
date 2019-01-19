780dcdeca3c2c029a2a9b3bcd84b7e053c164f2c
Ubuntu <ubuntu@ip-172-31-35-98.us-west-2.compute.internal>
2019-01-19 09:10:42 +0000

    load latest trained model

--- scalable_keras_redis/path_config.py
+++ scalable_keras_redis/path_config.py
@@ -1,9 +1,8 @@
-alcohol_h5_file = './alcohol_model/alcohol_inceptionV3_traininable_true_v1.h5'
-alcohol_json_file = './alcohol_model/alcohol_inceptionV3_traininable_true_v1.json'
+alcohol_h5_file = './alcohol_model/alcohol_ensemble_cnn_retain_v1.h5'
+alcohol_json_file = './alcohol_model/alcohol_ensemble_cnn_retain_v1.json'
 
-gambling_h5_file = './gambling_model/gambling_inceptionV3_traininable_true_v1.h5'
-gambling_json_file = './gambling_model/gambling_inceptionV3_traininable_true_v1.json'
+gambling_h5_file = './gambling_model/gambling_ensemble_cnn_rp.h5'
+gambling_json_file = './gambling_model/gambling_ensemble_cnn_rp_v4.json'
 
-ublock_firefox = r'C:\Users\User\AppData\Roaming\Mozilla\Firefox\Profiles\8e5i7n14.default-1545144909255\extensions\uBlock0@raymondhill.net.xpi'
-firefox_profile_director = r'C:\Users\User\AppData\Roaming\Mozilla\Firefox\Profiles'
-
+ublock_firefox = r'/home/ubuntu/.mozilla/firefox/8e5i7n14.default-1545144909255/extensions/uBlock0@raymondhill.net.xpi'
+firefox_profile_director = r'/home/ubuntu/.mozilla/firefox'
